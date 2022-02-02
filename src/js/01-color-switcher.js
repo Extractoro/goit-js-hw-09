6bba7b907b6557259bfec1b593c5b0fedb8f94df
Vadym Tytarenko (Extractoro) <67538058+Extractoro@users.noreply.github.com>
2022-02-02 17:28:23 +0200

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+/* Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body> на случайное значение используя инлайн стиль. 
+При нажатии на кнопку «Stop», изменение цвета фона должно останавливаться.
 
+⚠️ Учти, на кнопку «Start» можно нажать бесконечное количество раз. 
+Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна (disabled). */
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+const COLOR_CHANGE_DELAY = 1000;
+let timeoutid = null;
+
+btnStart.addEventListener('click', onStartClick)
+btnStop.addEventListener('click', onStopClick)
+
+function onStartClick() {
+    timeoutid = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, COLOR_CHANGE_DELAY);
+
+    btnStart.setAttribute('disabled', 'disabled')
+    btnStop.removeAttribute('disabled')
+}
+
+function onStopClick() {
+    clearInterval(timeoutid)
+
+    btnStart.removeAttribute('disabled')
+    btnStop.setAttribute('disabled', 'disabled')
+}
